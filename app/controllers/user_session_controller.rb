c63ce0f55e9f8dafa3523a4fb46990715a252ca1
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2018-12-21 22:48:31 +0700

    add devise

--- app/controllers/user_session_controller.rb
+++ app/controllers/user_session_controller.rb
@@ -1,11 +1,12 @@
 class UserSessionController < ApplicationController
   def create
-    user = User.find_by(email: params[:user][:email])
-    if user.valid_password?(params[:user][:password])
-      sign_in(user)
+    @user = User.find_by(email: params[:user][:email])
+    if @user&.valid_password?(params[:user][:password])
+      sign_in(@user)
       redirect_to root_path
     else
-      { message: 'Invalid email or password', code: 'signin_fail', status: 'error' }
+      redirect_to root_path
+      flash[:success] = "Email and Password invalid"
     end
   end
   
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def error_message(obj, key)
+    return if obj.errors.blank?
+    if key.is_a?(Array)
+      key.map { |k| content_tag(:p, obj.errors.full_messages_for(k).first, class: 'form-input-hint') if obj.errors.messages[k].present? }.compact.first
+    elsif obj.errors.present? && obj.errors.messages[key]
+      content_tag(:p, obj.errors.full_messages_for(key).first, class: 'form-input-hint')
+    end
+  end
 end
