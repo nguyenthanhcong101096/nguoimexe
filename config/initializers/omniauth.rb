1f38ef7abfc64aa072b68978edcb0ec7b0e1ccfc
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2018-12-21 23:08:10 +0700

    login with google

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.use OmniAuth::Builder do  
+  provider :google_oauth2,
+    '940492105837-g0qkk5hieu1ede677i5pceack1kttbmp.apps.googleusercontent.com',
+    'GczD38ybLyfNHKvXNkmYvkEk',
+    scope: "email"
+end
