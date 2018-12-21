707986cb36f335ead73fc10040e7321b1ac01d68
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2018-12-21 15:19:48 +0700

    login

--- app/javascript/scripts/application/common.js
+++ app/javascript/scripts/application/common.js
@@ -1,9 +1,13 @@
-const color = ['#df4a32', '#dd4b39', '#ff9926', '#888', '#3b5998', '#009966', '#CC3300']
-
-export const autoChangeColor = () => {
-  const listCard = document.querySelectorAll('.card')
-  listCard.forEach(card => {
-    var rand = Math.floor(Math.random() * Math.floor(6));
-    card.style.borderColor = color[rand];
+export const clickLoginPopup = () => {
+  const button = document.querySelector('.js-button-login');
+  const popup = document.querySelector('.login');
+  
+  button.addEventListener('click', () => {
+    popup.classList.remove('hidden')
+    
+    setTimeout(()=>{
+      popup.classList.add('animation')    
+    }, 200)
   })
 }
+
