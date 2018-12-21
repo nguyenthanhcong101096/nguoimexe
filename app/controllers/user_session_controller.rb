0f598c0ffccdbca96bb6ce0dc8ff2c1c64cf7187
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2018-12-21 16:05:54 +0700

    add gem devise

--- app/controllers/user_session_controller.rb
+++ app/controllers/user_session_controller.rb
@@ -3,13 +3,11 @@ class UserSessionController < ApplicationController
     user = User.find_by(email: params[:user][:email])
     if user.valid_password?(params[:user][:password])
       sign_in(user)
-      { message: 'Signed in', code: 'signed_in', status: 'ok' }
+      redirect_to root_path
     else
       { message: 'Invalid email or password', code: 'signin_fail', status: 'error' }
     end
   end
   
-  def destroy
-  
-  end
+  def destroy; end
 end
