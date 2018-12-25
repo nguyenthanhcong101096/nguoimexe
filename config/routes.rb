42d3c479eddd506884d9e94b2910ab638da6d9f3
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2018-12-25 10:52:45 +0700

    add activity and trackcomment

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  
   devise_for :users, skip: %i[sessions passwords], :controllers => { :omniauth_callbacks => "callbacks" }
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
   resources :posts, only: %i[show]
   resources :comments, only: %i[index create], defaults: { format: :html }
+  
+  mount Sidekiq::Web, at: '/sidekiq'
   
   post 'signin'    => 'user_session#create'
   get 'signout'    => 'user_session#destroy'
