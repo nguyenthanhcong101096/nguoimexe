0c109359883f016008676112604a59482ff7db60
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2018-12-26 11:34:47 +0700

    notfi

--- app/jobs/notification_job.rb
+++ app/jobs/notification_job.rb
@@ -1,14 +1,14 @@
 class NotificationJob < ApplicationJob
   queue_as :default
 
-  def perform(user, target_user, kind, url)
-    ActionCable.server.broadcast 'notifications_channel', html: render_notify(user, target_user, kind, url)
+  def perform(current_user, target_user, kind, url)
+    ActionCable.server.broadcast 'notifications_channel', channel_user: target_user.id, html: render_notify(current_user, target_user, kind, url)
   end
   
   private
   
-  def render_notify (user, target_user, kind, url)
-    activity = Activity.track(user, target_user, kind, url)
+  def render_notify (current_user, target_user, kind, url)
+    activity = Activity.track(current_user, target_user, kind, url)
     html = "<p>#{activity.message}</p>"
   end
 end
