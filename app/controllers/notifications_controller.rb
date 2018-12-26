0fa72853ec5f0893817f7d573606a85a7002465b
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2018-12-26 10:44:57 +0700

    nguyen thanh cong

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,5 @@
 class NotificationsController < ApplicationController
   def index
-    @activities = Activity.where(target_user: current_user)
+    @activities = Activity.where(target_user: current_user, read: false)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
   
   delegate :username, :avatar_url, to: :user, prefix: true
-  after_create_commit { NotificationJob.perform_later('Có 1 bình luận mới')}  
   after_create_commit { RenderCommentJob.perform_later self }
   def created_date
     created_at.strftime('%d %b. %Y')
