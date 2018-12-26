621dc490c282777a02426639e0cc7dcfcd528345
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2018-12-26 16:17:47 +0700

    design page show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   before_action :set_post, only: %i[index create]
   def index
-    @comments = @post.comments.where('id < ?', params[:min_id]).limit(5).order(id: :desc)
+    @comments = @post.comments.where('id < ?', params[:min_id]).limit(10).order(id: :desc)
     html_blocks = @comments.each_with_object([]) { |comment, arr| arr << render_to_string(partial: 'posts/comment', locals: { comment: comment }) }
     render json: { comments: html_blocks, min_id: @comments.last.id, max_id: @post.comments.first.id }, status: :ok
   end
