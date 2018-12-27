2e94d0550c4eb561de9f08394651432ec94d3208
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2018-12-27 20:55:43 +0700

    fix comment

--- app/jobs/render_comment_job.rb
+++ app/jobs/render_comment_job.rb
@@ -2,7 +2,7 @@ class RenderCommentJob < ApplicationJob
   queue_as :default
 
   def perform(comment)
-    ActionCable.server.broadcast 'comments_channel', html: render_new_comment(comment)
+    ActionCable.server.broadcast 'comments_channel', comment_channel: comment.post.id, html: render_new_comment(comment)
   end
   
   private
