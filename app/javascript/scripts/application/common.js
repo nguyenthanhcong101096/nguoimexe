0884ccb4b73b37c7f5fb0ed38a232c09d738cf1e
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-02 14:04:04 +0700

    post show

--- app/javascript/scripts/application/common.js
+++ app/javascript/scripts/application/common.js
@@ -1,7 +1,9 @@
 export const clickChangeLanguage = () => {
   const btn = document.querySelector('.js-language-switcher')
+  if(!btn) return
+  
   const locale = btn.getAttribute('locale')
-
+   
   btn.addEventListener('click', () => {
     window.location = locale
   })
