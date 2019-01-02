7b711bd66d58d73c9adf653af23ec913ca40f40f
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-02 19:35:04 +0700

    markup messages

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   resources :posts, only: %i[new create show]
   resources :comments, only: %i[index create], defaults: { format: :html }
   resources :follows, only: %i[create destroy], param: :user_id
-
+  resources :messages, only: %i[index show]
+  
   resources :users, only: %i[show edit update] do
     get 'change_password'   => 'users#change_password', on: :member
     patch 'set_password'    => 'users#set_password', on: :member
