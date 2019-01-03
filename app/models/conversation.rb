da7c9f0ab75b3f514c4e2a0caa04aa8fa6b60acf
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-03 09:17:11 +0700

    replace data conversation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,8 +3,8 @@ class Conversation < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :group_chats, through: :user_chats, source: :user
   
-  def chat_with_user
-    group_chats.last
+  def with_user user
+    group_chats.reject { |usr| usr == user }[0]
   end
   
   def last_message
