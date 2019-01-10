e9c5e2f2261faaa3476ccf4e3f889299e986dbc7
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-10 14:37:30 +0700

    deploy to aws

--- config/deploy.rb
+++ config/deploy.rb
@@ -95,5 +95,16 @@ namespace :deploy do
       end
     end
   end
+  
+  desc 'Run rake yarn:install'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install")
+      end
+    end
+  end
 end
+
+before "deploy:assets:precompile", "deploy:yarn_install"
 
