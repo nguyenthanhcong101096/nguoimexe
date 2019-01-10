22ecc1cd40dffc57ace0c679ad77e46362e73ec9
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-10 15:04:46 +0700

    deploy to aws

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,7 +59,7 @@ namespace :npm do
   task :install do
     on roles(:app) do
       within release_path do
-        execute("cd #{release_path} && npm install")
+        execute("cd #{release_path} && yarn install")
       end
     end
   end
