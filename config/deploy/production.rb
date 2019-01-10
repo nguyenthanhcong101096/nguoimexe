628a705a9f14470cf54123cee78a81a7caeb7e93
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-10 10:14:39 +0700

    deploy to aws

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 
 server '13.114.160.74', user: 'deploy', roles: %w{web app db}
-
+set :ssh_options, forward_agent: true
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
