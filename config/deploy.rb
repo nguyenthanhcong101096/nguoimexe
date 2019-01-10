83911585fcd044aafa7678c0bea99841968c2308
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-10 14:39:06 +0700

    deploy to aws

--- config/deploy.rb
+++ config/deploy.rb
@@ -99,8 +99,8 @@ namespace :deploy do
   desc 'Run rake yarn:install'
   task :yarn_install do
     on roles(:web) do
-      within release_path do
-        execute("cd #{release_path} && yarn install")
+      within current_path do
+        execute("cd #{current_path} && yarn install")
       end
     end
   end
