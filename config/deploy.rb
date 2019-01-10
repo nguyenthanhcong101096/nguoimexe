1eccf9ea6d44346d32c7cf979f6456b1908e3396
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-10 10:17:14 +0700

    deploy to aws

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,7 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
 
+ssh_options[:forward_agent] = true
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
