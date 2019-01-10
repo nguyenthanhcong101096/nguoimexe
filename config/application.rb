4c5632099e081d9babb995001e9b8b8a296693ef
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-10 23:44:08 +0700

    deploy to aws

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Nguoimexe
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.active_record.protected_environments = %w(staging)
     config.assets.paths << Rails.root.join('app', 'assets', 'files')
     config.assets.paths << Rails.root.join('node_modules')
     config.assets.precompile += %w[pghero/favicon.png]
