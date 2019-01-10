e93667e3845e48624b3f91dff13c74cefff5b6d8
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-10 23:41:04 +0700

    deploy to aws

--- config/application.rb
+++ config/application.rb
@@ -6,16 +6,13 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
 
 module Nguoimexe
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    
-    ActiveRecord::Base.protected_environments = %w(production)
-    
+    config.active_record.protected_environments = %w(staging)
     config.assets.paths << Rails.root.join('app', 'assets', 'files')
     config.assets.paths << Rails.root.join('node_modules')
     config.assets.precompile += %w[pghero/favicon.png]
