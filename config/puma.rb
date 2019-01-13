fc153e1a47657dc5fef844b3154bf83aca5eca9b
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-13 21:33:09 +0700

    config cable

--- config/puma.rb
+++ config/puma.rb
@@ -6,16 +6,19 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum, this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
-threads threads_count, threads_count
+
+# threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
+# threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-port        ENV.fetch('PORT') { 3000 }
+
+# port        ENV.fetch('PORT') { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch('RAILS_ENV') { 'development' }
+
+# environment ENV.fetch('RAILS_ENV') { 'development' }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -46,4 +49,14 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 # end
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+
+# plugin :tmp_restart
+
+
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads threads_count, threads_count
+bind "unix:/home/myapp/tmp/bluebird2.sock"
+environment ENV.fetch("RAILS_ENV") { "production" }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+daemonize true
+pidfile '/home/myapp/tmp/pids/puma.pid'
