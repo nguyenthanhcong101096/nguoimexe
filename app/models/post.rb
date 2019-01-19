0652297001e1a8187d4d5cca355342b90b3eec06
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-20 01:00:23 +0700

    rul

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,8 @@ class Post < ApplicationRecord
 
   delegate :username, :id, :avatar_url, to: :user, prefix: true
   delegate :name, to: :vehicle_kind, prefix: true
-
+  include ImageUploader::Attachment.new(:featured_image)
+  
   def created_date
     created_at.strftime('%d %b. %Y')
   end
