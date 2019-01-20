4e78f79998b6199e076a65fc55b0cbc5fe12bd68
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-20 15:53:57 +0700

    remove react

--- Gemfile
+++ Gemfile
@@ -25,7 +25,6 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 
 gem 'webpacker', '~> 3.5'
-gem 'react-rails'
 
 gem 'active_model_serializers', '~> 0.10.0'
 gem 'pg_search'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,3 @@ import '../styles/application.scss'
 import '../scripts/application'
 
 require.context('../images/', true)
-// Support component names relative to this directory:
-var componentRequireContext = require.context("components", true)
-var ReactRailsUJS = require("react_ujs")
-ReactRailsUJS.useContext(componentRequireContext)
