8235eb765ff8a62bc6c1d44cfd05ac2ac634b523
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-01-21 09:11:40 +0700

    SEO

---
+++ config/sitemap.rb
@@ -0,0 +1,8 @@
+SitemapGenerator::Sitemap.default_host = 'https://nguoimexe.com'
+SitemapGenerator::Sitemap.compress = false
+
+SitemapGenerator::Sitemap.create do
+
+
+  add root_path, priority: 0.8
+end
