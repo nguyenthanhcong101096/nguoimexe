a63072cc618a43cf50523f7ffabae1f003e56603
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-02-19 13:37:47 +0700

    run rubocop

--- app/channels/stream_comment_channel.rb
+++ app/channels/stream_comment_channel.rb
@@ -2,21 +2,21 @@
 
 class StreamCommentChannel < ApplicationCable::Channel
   include TrackNotificationActivity
-  
+
   def subscribed
     stream_from 'comments_channel'
   end
 
   def unsubscribed; end
-  
+
   def create_comment(data)
     post = Post.find(data['post_id'])
-    current_user.comments.create(context: data['message'],  commentable: post)
+    current_user.comments.create(context: data['message'], commentable: post)
     push_notification(current_user, post.user, 'comment', "https://nguoimexe.com/posts/#{post.slug_title}")
   end
-  
+
   private
-  
+
   def current_user
     User.find(user_id)
   end
