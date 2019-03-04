b1333e3faf26fcca01cdf867c20ce3074c69748b
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-03-04 16:37:59 +0700

    kaminari

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@
 
 class DashboardController < ApplicationController
   def index
-    @posts = Post.with_kind(params[:type])
+    @posts = Post.with_kind(params[:type]).page(params[:page]).per(5)
+    @data_load_more = { kind: params[:type], next_page:  @posts.next_page }
   end
 end
