fe98955632fab3dd50962501da98af92ee9941a9
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-03-09 16:37:57 +0700

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,11 +97,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'nguoimexe.com', port: 433 }
 
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'nguoimexe.com' }
-  
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
@@ -109,7 +107,7 @@ Rails.application.configure do
     user_name: ENV['gmail_username'],
     password: ENV['gmail_password'],
     authentication: 'plain',
-    domain: 'nguoimexe.com',
+    domain: 'nguoimexe.com'
     enable_starttls_auto: true
   }
 end
