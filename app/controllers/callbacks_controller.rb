f766cf8754d366e25f11420328b3884b6660fad1
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-03-11 14:04:30 +0700

    fb

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -6,4 +6,10 @@ class CallbacksController < ApplicationController
     cookies.signed[:user_id] = user.id
     sign_in_and_redirect user
   end
+  
+  def facebook
+    user = User.from_omniauth(request.env['omniauth.auth'])
+    cookies.signed[:user_id] = user.id
+    sign_in_and_redirect user
+  end
 end
