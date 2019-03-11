63f664e5eee62f704440f14f04332ef9eb128716
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-03-11 15:28:29 +0700

    login

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,14 +2,24 @@
 
 class CallbacksController < ApplicationController
   def google_oauth2
-    user = User.from_omniauth(request.env['omniauth.auth'])
-    cookies.signed[:user_id] = user.id
-    sign_in_and_redirect user
+    generic_callback
   end
   
   def facebook
-    user = User.from_omniauth(request.env['omniauth.auth'])
-    cookies.signed[:user_id] = user.id
-    sign_in_and_redirect user
+    generic_callback
+  end
+  
+  private
+  
+  def generic_callback
+    @identity = User.from_omniauth(request.env["omniauth.auth"])
+
+    user = @identity || current_user
+    if user.persisted?
+      cookies.signed[:user_id] = user.id
+      sign_in_and_redirect user
+    else
+      redirect_to root_path
+    end
   end
 end
