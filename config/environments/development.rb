4c639b68f0e3439db508c6964e629539815132f8
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-03-17 12:43:47 +0700

    sm

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = true 
   
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
