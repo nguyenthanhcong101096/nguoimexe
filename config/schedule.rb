c2a9d87473b9b281bf17df1f98d2d01f06f79dd8
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-03-17 09:36:41 +0700

    whener

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,15 +6,8 @@
 # Example:
 #
 set :output, "/home/deploy/nguoimexe/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-every 2.hours do
+
+every 12.hours do
   command "PGPASSWORD='password' pg_dump -F c -v -U nguoimexe -h localhost nguoimexe_production -f /home/deploy/nguoimexe/nguoimexe_backup.psql"
 end
 
-# Learn more: http://github.com/javan/whenever
