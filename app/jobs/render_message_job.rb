172175ecf660c2f35f3e8f372313a38eed6cd2e7
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-03-25 14:57:19 +0700

    update message

--- app/jobs/render_message_job.rb
+++ app/jobs/render_message_job.rb
@@ -4,8 +4,8 @@ class RenderMessageJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcast channel(message.conversation_id), user_id: message.sender_id.to_s, last_message: message.msg, right_html: right_message(message), left_html: left_message(message)
-    ActionCable.server.broadcast 'messages_channel', html: noti_message(message)[:html], target_user: noti_message(message)[:target_user].to_s
+    ActionCable.server.broadcast channel(message.conversation_id), render_message(message)
+    ActionCable.server.broadcast 'messages_channel', noti_message(message)
   end
 
   private
@@ -20,12 +20,18 @@ class RenderMessageJob < ApplicationJob
 
   def noti_message(message)
     conversation = message.conversation
+    room_chat    = message.conversation_id.to_s
     target_user  = conversation.with_user(message.sender)
-
+    
     html = ApplicationController.renderer.render(partial: 'shared/message', locals: { message: conversation, user: conversation.with_user(message.sender) })
-    { target_user: target_user.id, html: html }
+    
+    { target_user: target_user.id.to_s, html: html, room_chat: room_chat }
   end
-
+  
+  def render_message(message)
+    { user_id: message.sender_id.to_s, last_message: message.msg, right_html: right_message(message), left_html: left_message(message) }
+  end
+  
   def channel(room_id)
     "message_channel_#{room_id}"
   end
