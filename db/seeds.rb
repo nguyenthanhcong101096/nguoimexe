9fc8515c74d1fc90c79ce3a0c6319fbb5cfe4f57
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-04-15 00:22:20 +0700

    typing messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,14 +24,17 @@ comments_blog = 50.times { comment = FactoryBot.create(:comment, user: User.all.
 conversation_1  = FactoryBot.create(:conversation, name: 'thien-kim')
 conversation_2  = FactoryBot.create(:conversation, name: 'thanh-cong')
 conversation_3  = FactoryBot.create(:conversation, name: 'test1')
+conversation_4  = FactoryBot.create(:conversation, name: 'groups')
 
 room_1 = [hola, holo].each { |user| FactoryBot.create(:room_chat, sender_id: user.id, conversation_id: conversation_1.id) }
 room_2 = [hola, helo].each { |user| FactoryBot.create(:room_chat, sender_id: user.id, conversation_id: conversation_2.id) }
 room_3 = [holo, helo].each { |user| FactoryBot.create(:room_chat, sender_id: user.id, conversation_id: conversation_3.id) }
+room_4 = [holo, helo, hola].each { |user| FactoryBot.create(:room_chat, sender_id: user.id, conversation_id: conversation_4.id) }
 
 messages_room_1 = 15.times { FactoryBot.create(:message, conversation_id: conversation_1.id, sender_id: [hola, holo].sample.id) }
 messages_room_2 = 15.times { FactoryBot.create(:message, conversation_id: conversation_2.id, sender_id: [hola, helo].sample.id) }
 messages_room_3 = 15.times { FactoryBot.create(:message, conversation_id: conversation_3.id, sender_id: [holo, helo].sample.id) }
+messages_room_4 = 15.times { FactoryBot.create(:message, conversation_id: conversation_4.id, sender_id: [hola, holo, helo].sample.id) }
 
 
 # ENTERPRISE
