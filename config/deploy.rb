409de8680fe3fe13f5548875a705caaf376634af
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2019-07-19 20:48:27 +0700

    tmp

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,66 +1,46 @@
 # frozen_string_literal: true
 
-# config valid for current version and patch releases of Capistrano
-rvm_ruby = File.read('.ruby-version').strip
-lock '~> 3.6.1'
-
 # config valid only for current version of Capistrano
+lock '3.6.1'
 
-set :application, 'nguoimexe'
-set :repo_url, 'git@github.com:nguyenthanhcong101096/nguoimexe.git'
-set :branch, :master
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-set :deploy_to, '/home/deploy/nguoimexe'
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+set :scm, :git
+
+# Default value for :format is :airbrussh.
+set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+set :pty, false
+
+# Default value for :linked_files is []
+set :linked_files, ['config/database.yml', 'config/master.key']
+
+# Default value for linked_dirs is []
+set :linked_dirs, ['log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'bundle', 'public/packs', 'config/puma', 'node_modules', 'public/uploads']
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 3
+set :local_user, 'congnt'
 set :use_sudo, false
 
-# capistrano-rails
-set :rails_env, :production
-set :migration_role, :web
-
-set :assets_prefix, 'static'
-set :rails_assets_groups, :assets
-# If you need to touch public/images, public/javascripts, and public/stylesheets on each deploy
-set :normalize_asset_timestamps, ['public/static']
-set :keep_assets, 3
-
-set :pty, true
-set :linked_files, %w[config/database.yml config/application.yml]
-set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads public/app node_modules app/javascripts]
-set :keep_releases, 5
-set :rvm_type, :user
-set :rvm_ruby_version, rvm_ruby
-
-# capistrano/bundler
-set :bundle_binstubs, -> { shared_path.join('bin') }
-set :bundle_path, -> { shared_path.join('bundle') }
-set :bundle_without, %w[development test].join(' ')
-set :bundle_jobs, 4
-set :bundle_flags, '--deployment --quiet'
-
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock" # accept array for multi-bind
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 8]
-set :puma_workers, 0
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true
-set :puma_preload_app, false
-
-# Global options
-set :ssh_options, forward_agent: true
-
-namespace :npm do
-  desc 'Run rake npm install'
+namespace :yarn do
+  desc 'Run rake yarn install'
   task :install do
     on roles(:web) do
       within release_path do
-        execute("cd #{release_path} && npm install")
+        execute("cd #{release_path} && yarn install")
       end
     end
   end
@@ -70,18 +50,18 @@ namespace :deploy do
   desc 'Upload yml file.'
   task :upload_yml do
     on roles(:app) do
+      execute "mkdir -p #{shared_path}/config"
       execute "mkdir -p #{shared_path}/config/puma"
-      execute "mkdir -p #{shared_path}/node_modules"
-      upload!('package.json', "#{shared_path}/package.json")
+      upload!("config/puma/#{fetch(:stage)}.rb", "#{shared_path}/config/puma/#{fetch(:stage)}.rb")
       upload!('config/database.yml', "#{shared_path}/config/database.yml")
-      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+      upload!('config/master.key', "#{shared_path}/config/master.key")
     end
   end
 
   desc 'Seed the database.'
-  task :db_seed do
+  task :seed_db do
     on roles(:app) do
-      within release_path do
+      within current_path do
         with(rails_env: fetch(:stage)) do
           execute :bundle, :exec, :rake, 'db:seed'
         end
@@ -90,82 +70,22 @@ namespace :deploy do
   end
 
   desc 'Reset database'
-  task :db_reset do
+  task :reset_db do
     on roles(:app) do
-      within release_path do
+      within current_path do
         with(rails_env: fetch(:stage)) do
-          execute :rake, 'db:migrate:reset'
-        end
-      end
-    end
-  end
-
-  desc 'Backup database'
-  task :db_backup do
-    on roles(:app) do
-      within release_path do
-        with(rails_env: fetch(:stage)) do
-          execute 'PGPASSWORD="password" pg_dump -F c -v -U nguoimexe -h localhost nguoimexe_production -f /home/deploy/nguoimexe/nguoimexe_backup.psql'
-        end
-      end
-    end
-  end
-
-  desc 'Restore database'
-  task :db_restore do
-    on roles(:app) do
-      within release_path do
-        with(rails_env: fetch(:stage)) do
-          execute 'PGPASSWORD="password" pg_restore -d nguoimexe_production -h localhost /home/deploy/nguoimexe/nguoimexe_backup.psql -c -U nguoimexe'
-        end
-      end
-    end
-  end
-
-  desc 'Update Crontab'
-  task :update_crontab do
-    on roles(:app) do
-      within release_path do
-        with(rails_env: fetch(:stage)) do
-          execute "cd #{release_path} && whenever --update-crontab nguoimexe"
-        end
-      end
-    end
-  end
-
-  desc 'Log Production'
-  task :log do
-    on roles(:app) do
-      within release_path do
-        with(rails_env: fetch(:stage)) do
-          execute 'cd nguoimexe/shared/log && tail -f production.log'
+          execute :bundle, :exec, :rake, 'db:drop db:create db:migrate'
         end
       end
     end
   end
 end
 
-namespace :maintenance do
-  desc 'Switch current project into maintenance mode'
-  task :enable do
-    on primary :db do
-      within release_path do
-        execute 'cd nguoimexe && mv maintenance_off.html maintenance_on.html'
-      end
-    end
-  end
-
-  desc 'Turn off current project maintenance mode'
-  task :disable do
-    on primary :db do
-      within release_path do
-        execute 'cd nguoimexe && mv maintenance_on.html maintenance_off.html'
-      end
-    end
+task :log do
+  on roles(:app) do
+    execute "cd #{shared_path}/log && tail -f #{fetch(:stage)}.log"
   end
 end
 
-before('deploy:assets:precompile', 'npm:install')
 
-# pg_dump -F c -v -U postgres -h localhost nguoimexe_development -f ~/Desktop/nguoimexe/test_db.psql
-# pg_restore -d nguoimexe_development ~/Desktop/nguoimexe/test_db.psql -c -U postgres
+before('deploy:assets:precompile', 'yarn:install')
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,52 @@
 # frozen_string_literal: true
 
-server '54.255.207.227', user: 'deploy', roles: %w[web app db]
+rbenv_ruby = File.read('.ruby-version').strip
+
+set :stage, :production
+server '13.231.194.111', user: 'deploy', roles: %w[app web]
+
+set :application, 'wakuwaku'
+set :repo_url, 'git@github.com:1PACVietnam/wakuwaku.git'
+
+# Default branch is :master
+set :branch, 'staging'
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/var/www/wakuwaku.today/public_html'
+
+# capistrano-rails
+set :rails_env, :production
+set :migration_role, :web
+
+set :assets_prefix, 'packs'
+set :rails_assets_groups, :assets
+# If you need to touch public/images, public/javascripts, and public/stylesheets on each deploy
+set :normalize_asset_timestamps, ['public/packs']
+set :keep_assets, 3
+
+# capistrano-rbenv
+set :rbenv_type, :user
+set :rbenv_ruby, rbenv_ruby
+# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+# set :rbenv_map_bins, %w[rake gem bundle ruby rails puma pumactl sidekiq sidekiqctl]
+# set :rbenv_roles, :all # default value
+
+# capistrano/bundler
+set :bundle_binstubs, -> { shared_path.join('bin') }
+set :bundle_path, -> { shared_path.join('bundle') }
+set :bundle_without, %w[development test].join(' ')
+set :bundle_jobs, 4
+set :bundle_flags, '--deployment --quiet'
+
+# capistrano/puma
+set :puma_user, fetch(:user)
+set :puma_conf, -> { "#{shared_path}/config/puma/production.rb" }
+set :puma_role, :web
+set :puma_workers, 2
+
+# capistrano/sidekiq
+set :sidekiq_config, 'config/sidekiq.yml'
+
+# Global options
+# --------------
+set :ssh_options, forward_agent: true
